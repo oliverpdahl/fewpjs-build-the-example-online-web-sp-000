8975430984f175cc04a9baf46bd9f78492e7db3a
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-11 15:26:05 +0000

    Done.

--- main.js
+++ main.js
@@ -1,9 +1,52 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+let errorStatus = false
 
 // Your JavaScript code goes here!
-
+document.addEventListener("DOMContentLoaded", function(){
+  function setErrorModal(errorMessage = ''){
+    let errorModal = document.getElementById('modal');
+    let errorModalMessage = document.getElementById('modal-message');
+    errorStatus = !errorStatus
+    if(errorStatus){
+      errorModal.className = ''
+      errorModalMessage.innerText = errorMessage;
+      setTimeout(setErrorModal, 5000)
+    } else {
+      errorModal.className = 'hidden';
+    }
+  }
+  
+  function flipHeart(heart){
+     if(heart.className == 'activated-heart'){
+       heart.className = '';
+       heart.innerText = EMPTY_HEART;
+     } else {
+       heart.className = 'activated-heart';
+       heart.innerText = FULL_HEART;
+     }
+  }
+  
+  let posts = document.getElementsByClassName('media-post')
+  function setHeartLike(post){
+    let heartGlyph = post.getElementsByClassName('like-glyph')[0];
+    let heartContainer = post.getElementsByClassName('like')[0];
+    heartContainer.addEventListener('click', () =>{
+      mimicServerCall()
+      .then(() => {
+        flipHeart(heartGlyph);
+      })
+      .catch((error) => {
+        setErrorModal(error)
+      })
+    })
+  }
+  
+  for(const post of posts){
+    setHeartLike(post)
+  }
+})
 
 
 

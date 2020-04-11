cfb6cd0a89b25f7dd9337c5fc090e9be196cd0b7
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-11 14:50:16 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+let errorStatus = false
 
 // Your JavaScript code goes here!
-
+document.addEventListener("DOMContentLoaded", function(){
+  function setErrorModal(){
+    let errorModal = document.getElementById
+    if(errorStatus){
+      
+    } else {
+      
+    }
+  }
+})
 
 
 
